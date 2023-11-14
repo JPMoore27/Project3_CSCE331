2151e0bb51aec54984346bb94dc480102183be56
kaamishamsal14 <kaamishamsal14@gmail.com>
2023-11-14 15:23:56 -0600

    updated manager view with password and other changes

--- Kaamish/my-react-app/src/Manager.js
+++ Kaamish/my-react-app/src/Manager.js
@@ -1,28 +1,92 @@
+// Manager.js
 import React, { useState } from 'react';
+import './Manager.css'; // Import the CSS file for styling
 
 const Manager = () => {
-  // State for managing orders
+  const [password, setPassword] = useState('');
+  const [authenticated, setAuthenticated] = useState(false);
   const [orders, setOrders] = useState([]);
+  const [showCashierOptions, setShowCashierOptions] = useState(false);
+  const [showReportsOptions, setShowReportsOptions] = useState(false);
 
-  // Function to handle order submission
   const handleOrderSubmit = (order) => {
     setOrders([...orders, order]);
   };
 
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    // Replace 'manager123' with your desired password
+    if (password === 'manager123') {
+      setAuthenticated(true);
+    } else {
+      alert('Incorrect password. Please try again.');
+    }
+  };
+
+  const handleCashierButtonClick = () => {
+    setShowCashierOptions(!showCashierOptions);
+    setShowReportsOptions(false); // Close Reports options if open
+  };
+
+  const handleReportsButtonClick = () => {
+    setShowReportsOptions(!showReportsOptions);
+    setShowCashierOptions(false); // Close Cashier options if open
+  };
+
+  if (!authenticated) {
+    return (
+      <div className="password-container">
+        <h2>Manager Login</h2>
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h2>Manager Dashboard</h2>
+    <div className="manager-container">
+      {/* Heading buttons for Cashier and Reports */}
+      <div className="button-container">
+        <button className="heading-button" onClick={handleCashierButtonClick}>
+          Cashier
+        </button>
+        {showCashierOptions && (
+          <div className="sub-button-container">
+            <button className="sub-button">Check-In</button>
+            <button className="sub-button">Check-Out</button>
+            <button className="sub-button">Open Drawer</button>
+          </div>
+        )}
+        <button className="heading-button" onClick={handleReportsButtonClick}>
+          Reports
+        </button>
+        {showReportsOptions && (
+          <div className="sub-button-container">
+            <button className="sub-button">Inventory</button>
+            <button className="sub-button">Flash Reports</button>
+            <button className="sub-button">Sales Report</button>
+            <button className="sub-button">Cashier Sales</button>
+            <button className="sub-button">Labor Report</button>
+          </div>
+        )}
+      </div>
+
       {/* Display order management UI */}
       <div>
-        <h3>Orders</h3>
         <ul>
           {orders.map((order, index) => (
             <li key={index}>{order}</li>
           ))}
         </ul>
       </div>
-      {/* Component for managing menu items, inventory, etc. */}
-      {/* You can include more components for managing various aspects of the POS */}
     </div>
   );
 };
