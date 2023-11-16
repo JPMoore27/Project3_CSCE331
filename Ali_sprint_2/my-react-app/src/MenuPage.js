0b81872e56d1cf4854193ba344013c13cf932956
Alireza <a.mahdavi@tamu.edu>
2023-11-16 04:05:57 -0600

    * small fix

--- Ali_sprint_2/my-react-app/src/MenuPage.js
+++ Ali_sprint_2/my-react-app/src/MenuPage.js
@@ -4,7 +4,7 @@ import MenuList from './MenuList';
 const MenuPage = ({ showBackgroundImage }) => {
   return (
     <div className={`menu-page ${showBackgroundImage ? 'background-image' : ''}`}>
-      <h1>Menu</h1>
+      
       <MenuList />
     </div>
   );
