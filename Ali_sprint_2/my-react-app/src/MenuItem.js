da9507c53e1ca3f4635bd2eb04cd11ad8a937055
Alireza <a.mahdavi@tamu.edu>
2023-11-16 04:04:31 -0600

    * added flip animation

--- Ali_sprint_2/my-react-app/src/MenuItem.js
+++ Ali_sprint_2/my-react-app/src/MenuItem.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import './MenuItem.css';
 
 const MenuItem = ({ name, price, image, index }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold: 0.1,
   });
+
+  // Flip card handler
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
 
   // Assuming 5 items per row, adjust as needed
   const row = Math.floor(index / 5);
@@ -15,14 +21,19 @@ const MenuItem = ({ name, price, image, index }) => {
   return (
     <div
       ref={ref}
-      className={`menu-item ${inView ? 'in-view' : ''}`}
+      className={`menu-item ${inView ? 'in-view' : ''} ${isFlipped ? 'flipped' : ''}`}
       style={{ transitionDelay: `${inView ? delay : 0}ms` }}
+      onClick={handleFlip}
     >
-      <div className="menu-item-content">
+      <div className="menu-item-front">
         <img src={image} alt={name} className="menu-item-image" />
         <h3 className="menu-item-name">{name}</h3>
         <p className="menu-item-price">{price}</p>
-        <button className="menu-item-button">Add to cart</button> {/* Add this button */}
+        <button className="menu-item-button">Add to cart</button>
+      </div>
+      <div className="menu-item-back">
+        <h3>Ingredients</h3>
+        <p>Espresso, Milk, Sugar, Water</p> {/* Placeholder text */}
       </div>
     </div>
   );
