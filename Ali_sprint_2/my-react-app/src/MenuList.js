9b308e428c190af9e3eab7472d3fc4eda54bcb5d
Alireza <a.mahdavi@tamu.edu>
2023-11-16 16:06:18 -0600

    the cart works better.

--- Ali_sprint_2/my-react-app/src/MenuList.js
+++ Ali_sprint_2/my-react-app/src/MenuList.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import MenuItem from './MenuItem';
 import ShoppingCartPopup from './ShoppingCartPopup';
-import { addToCart, calculateTotal } from './Cart'; // Import other necessary dependencies
+import { calculateTotal } from './Cart'; // Import other necessary dependencies
 import './MenuItem.css';
 import coffeeCup from './assets/coffeeCup.png';
 import togoCoffeeCup from './assets/togoCoffeeCup.png'; // Import togoCoffeeCup
@@ -10,11 +10,11 @@ const menuData = [
   { itemName: "Coffee", price: 2.99 },
   { itemName: "Cappuccino", price: 5.65 },
   // ... (other menu items)
-  { itemName: "Iced Cafe Caramel Latte", price: 5.65 }
 ];
+
 const MenuList = () => {
   const [cart, setCart] = useState([]);
-  const [showCartPopup, setShowCartPopup] = useState(false);
+  const [showCartPopup, setShowCartPopup] = useState(false); // State to control the cart popup
 
   const getRandomImage = () => togoCoffeeCup;
 
@@ -26,27 +26,42 @@ const MenuList = () => {
     setShowCartPopup(false);
   };
 
+  const addToCart = (item) => {
+    // Check if the item is already in the cart
+    const existingItemIndex = cart.findIndex((cartItem) => cartItem.itemName === item.itemName);
+
+    if (existingItemIndex !== -1) {
+      // If the item exists in the cart, update its quantity
+      const updatedCart = [...cart];
+      updatedCart[existingItemIndex].quantity += 1;
+      setCart(updatedCart);
+    } else {
+      // If the item is not in the cart, add it with quantity 1
+      const newItem = { ...item, quantity: 1 };
+      setCart([...cart, newItem]);
+    }
+
+    handleShowCartPopup();
+  };
+
   return (
     <div>
       <div className="grid-container">
         {menuData.map((item, index) => (
           <MenuItem
-            key={item.itemName}
+            key={index}
             name={item.itemName}
-            price={item.price} // Make sure price is correctly set here
+            price={item.price}
             image={getRandomImage()}
             index={index}
-            addToCart={() => {
-              const newItem = { ...item, quantity: 1 };
-              setCart([...cart, newItem]);
-              handleShowCartPopup();
-            }}
+            addToCart={() => addToCart(item)}
           />
         ))}
       </div>
 
       <button onClick={handleShowCartPopup}>View Cart</button>
 
+      {/* Render the cart popup component when showCartPopup is true */}
       {showCartPopup && (
         <ShoppingCartPopup
           cart={cart}
--- Ali_sprint_2/my-react-app/src/ShoppingCartPopup.js
+++ Ali_sprint_2/my-react-app/src/ShoppingCartPopup.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
 const ShoppingCartPopup = ({ cart, onClose }) => {
+  // CSS styles for centering the shopping cart
+  const popupStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    background: 'rgba(0, 0, 0, 0.7)',
+    zIndex: 1000,
+  };
+
   return (
-    <div className="shopping-cart-popup">
-      <h3>Shopping Cart</h3>
-      <ul>
-        {cart.map((item, index) => (
-          <li key={index}>
-            {item.name} - ${item.price.toFixed(2)} x {item.quantity}
-          </li>
-        ))}
-      </ul>
-      <button onClick={onClose}>Close</button>
+    <div style={popupStyles}>
+      <div className="shopping-cart-popup">
+        <h3>Shopping Cart</h3>
+        <ul>
+            {cart.map((item, index) => (
+                <li key={index}>
+                    {item.itemName} - ${item.price.toFixed(2)} x {item.quantity}
+                </li>
+            ))}
+        </ul>
+        <button onClick={onClose}>Close</button>
+      </div>
     </div>
   );
 };
