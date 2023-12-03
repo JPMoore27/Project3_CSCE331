391e8cb8164d69a74140ab2bb7780d98f588ae97
Alireza <a.mahdavi@tamu.edu>
2023-12-02 18:59:41 -0600

    added_add_on_for menu

--- Sprint_3_Final/my-react-app/src/MenuPage.js
+++ Sprint_3_Final/my-react-app/src/MenuPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import MenuList from './MenuList';
+import AddOnSection from './AddOnSection';
 
 const MenuPage = ({ showBackgroundImage }) => {
   return (
     <div className={`menu-page ${showBackgroundImage ? 'background-image' : ''}`}>
-      
+      <AddOnSection />
       <MenuList />
     </div>
   );
