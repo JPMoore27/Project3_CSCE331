3def93a6fa957adfa563e82695de8c16481b7d4b
JPMoore27 <jp_moore@tamu.edu>
2023-12-03 10:53:32 -0600

    added post and delete commands to API

--- django/mysite/menu/urls.py
+++ django/mysite/menu/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from .views import ItemsApiView, AddonsApiView, MerchApiView, OrdersApiView, StockApiView
 
 urlpatterns = [
-    path('items/', ItemsApiView.as_view(), name='items-api'),
-    path('addons/', AddonsApiView.as_view(), name='addons-api'),
-    path('merch/', MerchApiView.as_view(), name='merch-api'),
-    path('orders/', OrdersApiView.as_view(), name='orders-api'),
-    path('stock/', StockApiView.as_view(), name='stock-api'),
+    path('items/', ItemsApiView.as_view(), name='items-list'),
+    path('items/<int:pk>/', ItemsApiView.as_view(), name='items-detail'),
+    
+    path('addons/', AddonsApiView.as_view(), name='addons-list'),
+    path('addons/<int:pk>/', AddonsApiView.as_view(), name='addons-detail'),
+
+    path('merch/', MerchApiView.as_view(), name='merch-list'),
+    path('merch/<int:pk>/', MerchApiView.as_view(), name='merch-detail'),
+
+    path('orders/', OrdersApiView.as_view(), name='orders-list'),
+    path('orders/<int:pk>/', OrdersApiView.as_view(), name='orders-detail'),
+
+    path('stock/', StockApiView.as_view(), name='stock-list'),
+    path('stock/<int:pk>/', StockApiView.as_view(), name='stock-detail'),
 ]
