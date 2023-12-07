134fba00f04788e489abfd1b971f4543a5b499d9
JPMoore27 <jp_moore@tamu.edu>
2023-12-07 16:04:27 -0600

    made orders table

--- apitest/my-api-app/src/ApiComponent.js
+++ apitest/my-api-app/src/ApiComponent.js
@@ -1,148 +1,75 @@
+// src/components/OrdersTable.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
-
-const ItemList = () => {
-  const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState({ itemname: '', price: 0, stockid: 0, dairy: false });
-  const [stockItems, setStockItems] = useState([]);
-  const [selectedStockItems, setSelectedStockItems] = useState([]);
+const OrdersTable = () => {
+  const [orders, setOrders] = useState([]);
+  const [items, setItems] = useState({});
 
   useEffect(() => {
-    fetchItems();
-    fetchStockItems();
+    // Fetch orders from your Django backend API and sort by orderid
+    axios.get('https://project3-team03g.onrender.com/api/orders/')
+      .then(response => {
+        const sortedOrders = response.data.sort((a, b) => a.orderid - b.orderid);
+        setOrders(sortedOrders);
+      })
+      .catch(error => console.error('Error fetching orders:', error));
+
+    // Fetch items from your Django backend API
+    axios.get('https://project3-team03g.onrender.com/api/items/')
+      .then(response => {
+        // Create a mapping of itemid to itemname for quick lookups
+        const itemMapping = {};
+        response.data.forEach(item => {
+          itemMapping[item.itemid] = item.itemname;
+        });
+        setItems(itemMapping);
+      })
+      .catch(error => console.error('Error fetching items:', error));
   }, []);
 
-  const fetchItems = async () => {
-    try {
-      const response = await axios.get(`${API_BASE_URL}/items/`);
-      setItems(response.data);
-    } catch (error) {
-      console.error('Error fetching items:', error);
-    }
+  const handleVoidClick = (orderId) => {
+    // Make a DELETE request to void the order
+    axios.delete(`https://project3-team03g.onrender.com/api/orders/${orderId}/`)
+      .then(() => {
+        // Remove the voided order from the local state
+        setOrders(prevOrders => prevOrders.filter(order => order.orderid !== orderId));
+      })
+      .catch(error => console.error('Error voiding order:', error));
   };
-
-  const fetchStockItems = async () => {
-    try {
-      const response = await axios.get(`${API_BASE_URL}/stock/`);
-      setStockItems(response.data);
-    } catch (error) {
-      console.error('Error fetching stock items:', error);
-    }
-  };
-
-  const findLowestAvailableKey = () => {
-    const keys = items.map(item => item.key);
-    return Math.max(0, ...keys) + 1; // Return 1 more than the highest in-use key
-  };
-
-  const findLowestAvailableItemId = () => {
-    const itemIds = items.map(item => item.itemid);
-    return Math.max(0, ...itemIds) + 1; // Return 1 more than the highest in-use itemid
-  };
-
-  const handleAddItem = async () => {
-    try {
-      const newItemId = findLowestAvailableItemId();
-      const startKey = findLowestAvailableKey(); // Find the starting key
-
-      // Create a new item for each selected stock item
-      const newItemPromises = selectedStockItems.map(async (stockItem, index) => {
-        const newKeyForEntry = startKey + index; // Generate a new key for each entry
-        const response = await axios.post(`${API_BASE_URL}/items/`, {
-          ...newItem,
-          key: newKeyForEntry,
-          itemid: newItemId,
-          stockid: stockItem.stockid,
-        });
-        return response.data;
-      });
-
-      // Wait for all new items to be created
-      const newItems = await Promise.all(newItemPromises);
-
-      setItems([...items, ...newItems]);
-      setNewItem({ itemname: '', price: 0, stockid: 0, dairy: false });
-      setSelectedStockItems([]); // Reset selected stock items
-    } catch (error) {
-      console.error('Error adding item:', error);
-    }
-  };
-
-  const handleDeleteItem = async (itemId) => {
-    try {
-      await axios.delete(`${API_BASE_URL}/items/${itemId}/`);
-      setItems(items.filter(item => item.key !== itemId));
-    } catch (error) {
-      console.error('Error deleting item:', error);
-    }
-  };
-
-  // Filter items to show only those with unique itemid
-  const uniqueItems = Array.from(new Set(items.map(item => item.itemid)))
-    .map(itemid => items.find(item => item.itemid === itemid));
 
   return (
-    <div>
-      <h2>Items List</h2>
-      <ul>
-        {uniqueItems.map(item => (
-          <li key={item.key}>
-            {item.itemname} - ${item.price}
-            <button onClick={() => handleDeleteItem(item.key)}>Delete</button>
-          </li>
+    <table>
+      <thead>
+        <tr>
+          <th>Order ID</th>
+          <th>Item Name</th>
+          <th>Quantity</th>
+          <th>Time</th>
+          <th>Customer Name</th>
+          <th>Takeout</th>
+          <th>Price</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {orders.map(order => (
+          <tr key={order.orderid}>
+            <td>{order.orderid}</td>
+            <td>{items[order.itemid]}</td>
+            <td>{order.quantity}</td>
+            <td>{order.time}</td>
+            <td>{order.customername}</td>
+            <td>{order.takeout ? 'Yes' : 'No'}</td>
+            <td>{order.price}</td>
+            <td>
+              <button onClick={() => handleVoidClick(order.orderid)}>Void</button>
+            </td>
+          </tr>
         ))}
-      </ul>
-      <h2>Add New Item</h2>
-      <div>
-        <label>
-          Item Name:
-          <input
-            type="text"
-            value={newItem.itemname}
-            onChange={(e) => setNewItem({ ...newItem, itemname: e.target.value })}
-          />
-        </label>
-        <label>
-          Price:
-          <input
-            type="number"
-            value={newItem.price}
-            onChange={(e) => setNewItem({ ...newItem, price: e.target.value })}
-          />
-        </label>
-        <label>
-          Dairy:
-          <input
-            type="checkbox"
-            checked={newItem.dairy}
-            onChange={(e) => setNewItem({ ...newItem, dairy: e.target.checked })}
-          />
-        </label>
-        <div>
-          <h3>Stock Items</h3>
-          {stockItems.map(stockItem => (
-            <div key={stockItem.stockid}>
-              <input
-                type="checkbox"
-                checked={selectedStockItems.some(item => item.stockid === stockItem.stockid)}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    setSelectedStockItems([...selectedStockItems, stockItem]);
-                  } else {
-                    setSelectedStockItems(selectedStockItems.filter(item => item.stockid !== stockItem.stockid));
-                  }
-                }}
-              />
-              {stockItem.stockname}
-            </div>
-          ))}
-        </div>
-        <button onClick={handleAddItem}>Add Item</button>
-      </div>
-    </div>
+      </tbody>
+    </table>
   );
 };
 
-export default ItemList;
+export default OrdersTable;
