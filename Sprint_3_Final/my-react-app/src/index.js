d51d699b451cbc31c2f89ce62284d7fa8f57ce8e
Jaein Cha <jaein.cha@tamu.edu>
2023-12-06 21:36:36 -0600

    updated google auth client id

--- Sprint_3_Final/my-react-app/src/index.js
+++ Sprint_3_Final/my-react-app/src/index.js
@@ -5,7 +5,10 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <GoogleOAuthProvider clientId="597620423298-3dhrtvache13kfgm2sssfj5lva695uit.apps.googleusercontent.com">
+//     <GoogleOAuthProvider clientId="597620423298-3dhrtvache13kfgm2sssfj5lva695uit.apps.googleusercontent.com">
+// <App />
+// </GoogleOAuthProvider>
+        <GoogleOAuthProvider clientId="364815935021-7idv75k5jm8fvk1e0gcml5rebqtluds3.apps.googleusercontent.com">
 <App />
 </GoogleOAuthProvider>
 );
